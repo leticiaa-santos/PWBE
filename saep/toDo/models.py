34f84feabea0c64910c9a5952f91b83cbccadb00
leticiaa-santos <43705577808@>
2025-08-22 09:34:47 -0300

    simulado - saep

---
+++ saep/toDo/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+PRIORIDADE_CHOICES = [
+    ('baixa', 'baixa'),
+    ('media', 'media'),
+    ('alta', 'alta')
+]
+
+STATUS_CHOICES = [
+    ('a fazer', 'a fazer'),
+    ('fazendo', 'fazendo'),
+    ('pronto', 'pronto')
+]   
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.nome
+    
+class Tafera(models.Model):
+    descricao = models.TextField()
+    nomeSetor = models.CharField(max_length=90)
+    prioridade = models.CharField(max_length=5, choices=PRIORIDADE_CHOICES)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='a fazer')
+    data_cadastro = models.DateField()
+    idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'O usuário {self.idUsuario} tem a tarefa {self.descricao}'
