1e326807ae2ba7592909897bad93607980cf3c19
leticiaa-santos <43705577808@>
2025-08-25 11:29:06 -0300

    pequenas mudanças nas models

--- simulado_saep/toDo/models.py
+++ simulado_saep/toDo/models.py
@@ -13,7 +13,7 @@ STATUS_CHOICES = [
 ]   
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=20)
+    nome = models.CharField(max_length=50)
     email = models.CharField(max_length=60)
 
     def __str__(self):
