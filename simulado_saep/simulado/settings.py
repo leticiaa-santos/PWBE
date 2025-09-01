2532a4cc3aff2b9034f5d99973b07f83c3a517ec
leticiaa-santos <43705577808@>
2025-09-01 11:36:44 -0300

    forms

--- simulado_saep/simulado/settings.py
+++ simulado_saep/simulado/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'toDo',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'simulado.urls'
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS =[
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+]
--- simulado_saep/toDo/models.py
+++ simulado_saep/toDo/models.py
@@ -9,22 +9,22 @@ PRIORIDADE_CHOICES = [
 STATUS_CHOICES = [
     ('a fazer', 'a fazer'),
     ('fazendo', 'fazendo'),
-    ('pronto', 'pronto')
+    ('feito', 'feito')
 ]   
 
 class Usuario(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.CharField(max_length=60)
+    nome = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
 
     def __str__(self):
         return self.nome
     
 class Tarefa(models.Model):
-    descricao = models.TextField()
+    descricao = models.TextField(max_length=255)
     nomeSetor = models.CharField(max_length=90)
     prioridade = models.CharField(max_length=5, choices=PRIORIDADE_CHOICES)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='a fazer')
-    data_cadastro = models.DateField()
+    data_cadastro = models.DateField(auto_now_add=True)
     idUsuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 
     def __str__(self):
